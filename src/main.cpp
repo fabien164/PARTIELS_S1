a012276271435b1c5e1e3905eb3979387fa7bc87
fabien164 <corfabien31mail.com>
2024-03-15 12:54:52 +0000

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int scootersTotal = 632;
+ int scootersMaintenance = 10;
+ int scootersDispos = 200;
+ const float coutMinute = 0.33;
+ cout << "****** Location de scooters électriques ******" << endl << endl;
+ cout << "----------------------------------------------" << endl;
+ //Partie à compléter 3.1
+ cout << "----------------------------------------------" << endl << endl;
+ cout << "1. Emprunter un scooter électrique" << endl;
+ cout << "2. Restituer un scooter électrique" << endl;
+ cout << "3. Estimer le coût de la location" << endl;
+ cout << "4. Quitter l'application" << endl << endl;
+ return 0;
+}
